b1abd1d006f1013019b9f96f007cbc7226875ec6
AnishaDahiya <anishadahiya@gmail.com>
2026-09-26 14:14:58 +0530

    Add learning resources page navigation

--- app.py
+++ app.py
@@ -497,7 +497,10 @@ def home():
     return render_template(
         "index.html"
     )
-
+    
+@app.route("/learning-resources")
+def learning_resources():
+    return render_template("learning_resources.html")
 
 # =========================================================
 # REGISTER
